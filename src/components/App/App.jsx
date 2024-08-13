4626afa47d6461aa125b72c2c3e21025f8f8fa0a
brayann graux <pics.prod45@gmail.com>
2024-08-13 12:16:00 +0200

    dernier push pour soutenance

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from '../../pages/Home/Home'
 import Error from '../../pages/Error/Error'
 import Logements from '../../pages/Logements/Logements'
@@ -9,16 +9,16 @@ import '../../style/style.scss'
 
 function App() {
   return (
-    <HashRouter>
-      <Header />
+  <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/logements/:id" element={<Logements />} errorElement={<Error />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Error />} />
       </Routes>
-      <Footer />
-    </HashRouter>
+    <Footer />
+  </BrowserRouter>
   )
 }
 
